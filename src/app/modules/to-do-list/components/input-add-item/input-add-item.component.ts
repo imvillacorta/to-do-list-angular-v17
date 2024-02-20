e8734f47e391e480896d0433f0a5b740fcf4c623
Ivan Villacorta <ivan_mv23@hotmail.com>
2024-02-20 17:17:34 -0300

    feat: deleting data from localstorage list

--- src/app/modules/to-do-list/components/input-add-item/input-add-item.component.ts
+++ src/app/modules/to-do-list/components/input-add-item/input-add-item.component.ts
@@ -1,16 +1,18 @@
-import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Output, ViewChild, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, Output, ViewChild, inject } from '@angular/core';
 import { IListItems } from '../../interface/IListItems.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-input-add-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './input-add-item.component.html',
   styleUrl: './input-add-item.component.scss'
 })
 export class InputAddItemComponent {
   #cdr = inject(ChangeDetectorRef);
   @ViewChild('inputText') public inputText!: ElementRef;
+  @Input({ required: true }) public inputListItems: IListItems[] = [];
   @Output() public outputAddListItem = new EventEmitter<IListItems>();
 
   public focusAndAddItem(value: string) {
@@ -28,7 +30,7 @@ export class InputAddItemComponent {
         value
       });
 
-    return this.inputText.nativeElement.focus();
+      return this.inputText.nativeElement.focus();
 
     }
   }
